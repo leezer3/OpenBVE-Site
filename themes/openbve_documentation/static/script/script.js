5c5a34c61bffb66447f8118b28a9813c16990368
S520 <29241703+s520@users.noreply.github.com>
2019-01-13 07:10:44 +0900

    Update: language selector without jQuery

--- themes/openbve_documentation/static/script/script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $(document).ready(function() {
-        var $select = $('.language-selector').select2({width: 'resolve'});
-    });
-
-    $('.language-selector').on('select2:select', function (e) {
-        location.href = e.params.data.id;
-    });
-});
